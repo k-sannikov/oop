2bf7103f720b142458d8d245fff57280607c0f0a
Kirill Sannikov <kir.sannikow@mail.ru>
2022-02-13 23:26:21 +0300

    Flip Byte

---
+++ lab1/FlipByte/FlipByte/FlipByte.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ErrorDisplay()
+{
+	cout << "Usage: FlipByte.exe <input byte>\n";
+	cout << "*<input byte> is number from 0 to 255\n";
+}
+
+bool IsStrNumber(string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return false;
+		}
+	}
+	return str != "";
+}
+
+int ParseArg(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		ErrorDisplay();
+		cout << "Invalid argument count\n";
+		return -1;
+	}
+
+	if (!IsStrNumber(static_cast<string>(argv[1])))
+	{
+		cout << "Invalid argument type\n";
+		ErrorDisplay();
+		return -1;
+	}
+
+	int number = stoi(static_cast<string>(argv[1]));
+
+	if ((number < 0) || (number > 255))
+	{
+		cout << "Invalid argument value\n";
+		ErrorDisplay();
+		return -1;
+	}
+
+	return number;
+}
+
+unsigned char FlipBits(unsigned char number)
+{
+	unsigned char result = 0x00;
+	for (int i = 1; number != 0; i++)
+	{
+		if (number & 0x01)
+		{
+			result |= (0x01 << (CHAR_BIT - i));
+		}
+		number >>= 1;
+	}
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	int number = ParseArg(argc, argv);
+
+	// Проверка правильности аргументов командной строки
+	if (number == -1)
+	{
+		return 1;
+	}
+
+	unsigned char result = FlipBits(static_cast<unsigned char>(number));
+
+	cout << static_cast<unsigned int>(result) << endl;
+
+	return 0;
+}
---
+++ lab1/FlipByte/FlipByte/Tests.bat
@@ -0,0 +1,55 @@
+@echo off
+
+REM Путь к тестируемой программе передается через 1-й аргумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+) 
+
+REM Тест 1: Запуск без аргументов
+%MyProgram% > nul && goto err
+echo Test 1 passed
+
+REM Тест 2: Запуск c лишними аргументов
+%MyProgram% 4 5 6 > nul && goto err
+echo Test 2 passed
+
+REM Тест 3: Запуск с передачей пустой строки
+%MyProgram% NonEmpty.txt "" > nul && goto err
+echo Test 3 passed
+
+REM Тест 4: Запуск с передачей невалидного типа данных
+%MyProgram% abc123 > nul && goto err
+echo Test 4 passed
+
+REM Тест 5: Запуск с передачей невалидного значения
+%MyProgram% -10 > nul && goto err
+echo Test 5 passed
+
+REM Тест 6: Запуск с передачей невалидного значения
+%MyProgram% 350 > nul && goto err
+echo Test 6 passed
+
+REM Тест 7: Запуск с передачей валидного значения
+%MyProgram% 6 > "%TEMP%\output.txt" || goto err
+set /p result=<"%TEMP%\output.txt"
+if not %result% == 96 goto err
+echo Test 7 passed
+
+REM Тест 8: Запуск с передачей валидного значения
+%MyProgram% 65 > "%TEMP%\output.txt" || goto err
+set /p result=<"%TEMP%\output.txt"
+if not %result% == 130 goto err
+echo Test 8 passed
+
+REM Тесты прошли успешно
+echo All tests passed successfull
+exit /B 0
+
+REM Переход сюда в случае ошибки
+:err
+echo Test failed
+exit /B 1
